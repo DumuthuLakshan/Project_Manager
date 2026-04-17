589a48f6adc0cad955d01aa15a89ddc8895fda12
DumuthuLakshan <lakshandumuthu@gmail.com>
2026-04-17 13:37:52 +0530

    Initial commit - register form with Node.js and MySQL

---
+++ server.js
@@ -0,0 +1,41 @@
+const express=require("express");
+const mysql= require("mysql2");
+const cors= require("cors");
+
+const app= express();
+
+app.use(cors());
+app.use(express.json());
+
+const db=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"Saas"
+});
+
+db.connect((err)=>{
+    if(err){
+        console.log("Database connection failed:", err);
+    }else{
+        console.log("Connected to MySQL");
+    }
+});
+
+app.post("/register", (req, res)=>{
+    const{UserID,Email,Password}=req.body;
+
+    const sql="INSERT INTO user(UserID,Email,Password) VALUES (?,?,?)";
+    db.query(sql,[UserID,Email,Password],(err,result)=>{
+        if (err){
+            console.log(err);
+            res.status(500).send("error saving data");
+        }else{
+            res.send("User registered successfully");
+        }
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("server running on http://localhost:3000");
+});
